59f506c7dc1ccc1bec4df05b114fc512e645b119
nkbhadja <nayanbhadja222@gmail.com>
2023-03-22 18:29:56 +0530

    table of  product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'boilerplate';
+
+  products = [
+    { name: 'Product A', price: 10.99, inStock: true, stock: 5 },
+    { name: 'Product B', price: 19.99, inStock: false, stock: 0 },
+    { name: 'Product C', price: 7.99, inStock: true, stock: 2 },
+    { name: 'Product D', price: 14.99, inStock: true, stock: 10 },
+    { name: 'Product E', price: 8.99, inStock: false, stock: 0 }
+  ];
 }
